18b3c446c6f9c641992cca8216653e6603d11553
ChoiBowon <bowon1202@gmail.com>
2018-04-10 22:30:00 +0900

    Part1 commit

---
+++ Tutorial/mysite/mysite/urls.py
@@ -0,0 +1,8 @@
+# mysite/urls.py
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
